7a5ea6c49285da893973600fa53e3a53b6523856
DariaSemikina <62110659+DariaSemikina@users.noreply.github.com>
2022-11-16 09:05:52 +0100

    Update settings.py
    Added comments on settings classes

--- config/settings.py
+++ config/settings.py
@@ -2,12 +2,14 @@ from pydantic import BaseSettings
 
 
 class QuerySettings(BaseSettings):
+    """The settings for the query insertion operation"""
     allow_large_results: bool = True
     destination: str = "funda-bi-dev.dbt_darya_adhoc.cows_descendants"
     use_legacy_sql: bool = False
     write_disposition: str = "WRITE_TRUNCATE"
 
 class ProjectSettings(BaseSettings):
+    """Project database settings, should be adjusted for the project usage"""
     project_id: str = "test-project"
     dataset: str = "test-dataset"
     table: str = "cows-table"
